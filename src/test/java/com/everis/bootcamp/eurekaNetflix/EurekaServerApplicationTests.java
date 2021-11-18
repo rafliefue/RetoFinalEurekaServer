3ec05d219e1231a502735fdece5099fb9edd1fb1
rafliefue <rafliefue@gmail.com>
2021-11-18 14:37:46 +0100

    Eureka Server

---
+++ src/test/java/com/everis/bootcamp/eurekaNetflix/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everis.bootcamp.eurekaNetflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
